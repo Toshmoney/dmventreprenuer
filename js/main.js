21f7f51ea2613d8cce45f2486b9390b44b197c26
Toshmoney <abdulmaleeqomotosho@gmail.com>
2024-06-09 18:11:10 +0100

    google translator

--- js/main.js
+++ js/main.js
@@ -136,3 +136,12 @@
 
 })(jQuery);
 
+document.getElementById("google_translate_element").style.borderColor='#f5207d'
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({
+        pageLanguage: 'en', 
+        includedLanguages: 'en,fr',
+        autoDisplay: false,
+        layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+    }, 'google_translate_element');
+}
